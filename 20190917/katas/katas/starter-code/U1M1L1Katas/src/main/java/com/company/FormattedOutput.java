08701bae083ccabd3e3fec6f7e01371d8d9a4ed2
Zach Merel <zachmerel@gmail.com>
2019-09-16 19:42:10 -0500

    Homework 20190917 tests complete

--- 20190917/katas/katas/starter-code/U1M1L1Katas/src/main/java/com/company/FormattedOutput.java
+++ 20190917/katas/katas/starter-code/U1M1L1Katas/src/main/java/com/company/FormattedOutput.java
@@ -3,6 +3,7 @@ package com.company;
 public class FormattedOutput {
 
     public static void main(String[] args) {
+        System.out.format("My name is %s and I am %d years old.", "Zach Merel", 26);
 
     }
 }
